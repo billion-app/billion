1702ecca8743524623ece6570c8fbbee8f66b90e
Yohaan Narayanan <229655281+yohaann196@users.noreply.github.com>
2026-04-05 11:25:47 -0700

    Refactor layout by removing ThemeProvider
    Removed ThemeProvider and ThemeToggle components from layout.

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -7,7 +7,6 @@ import {
 } from "next/font/google";
 
 import { cn } from "@acme/ui";
-import { ThemeProvider, ThemeToggle } from "@acme/ui/theme";
 import { Toaster } from "@acme/ui/toast";
 
 import { env } from "~/env";
@@ -79,13 +78,8 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           geistMono.variable,
         )}
       >
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <TRPCReactProvider>{props.children}</TRPCReactProvider>
-          <div className="absolute right-4 bottom-4">
-            <ThemeToggle />
-          </div>
-          <Toaster />
-        </ThemeProvider>
+        <TRPCReactProvider>{props.children}</TRPCReactProvider>
+        <Toaster />
       </body>
     </html>
   );
