6dbffb91f5b7866ef83c60c5f7859cb5354e4a69
ThatXliner <bryan.hu.2020@gmail.com>
2026-07-09 18:12:31 -0700

    chore: release 0.2.3

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -1,25 +1,9 @@
 // Learn more: https://docs.expo.dev/guides/monorepos/
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require("nativewind/metro");
-const path = require("path");
 
 const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "../..");
-
 const config = getDefaultConfig(projectRoot);
-
-// 1. Watch all files in the monorepo
-config.watchFolders = [workspaceRoot];
-
-// 2. Resolve packages from both the project and the workspace root
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules"),
-];
-
-// 3. Ensure Metro follows symlinks (required for pnpm workspaces)
-config.resolver.unstable_enableSymlinks = true;
-config.resolver.unstable_enablePackageExports = true;
 
 /** @type {import('expo/metro-config').MetroConfig} */
 module.exports = withNativewind(config);
